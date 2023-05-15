0f64fb6a07a13a9ae6ee19424a226bf6d0958dd6
Rufai T <52770296+Rufi-j@users.noreply.github.com>
2023-05-15 13:13:45 +0100

    html set-up

---
+++ src/components/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <nav>
+        <img src="assets/lemon-logo.png" alt="logo" />
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Menu</li>
+            <li>Reservations</li>
+            <li>Order Online</li>
+            <li>Login</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Nav
